c6afcb04e17d6b5874fe6b1fc3212cde8db287d4
inspectorfiby <phoebezcy93@gmail.com>
2018-03-13 16:00:49 -0400

    word count file

--- src/main/java/WordCount.java
+++ src/main/java/WordCount.java
@@ -1,16 +1,70 @@
 import java.io.IOException;
-import org.apache.hadoop.io.LongWritable;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
-public class WordcountMapper extends Mapper<LongWritable, Text, Text, Text> {
+public class WordCount {
 
-	@Override
-	protected void map(LongWritable key, Text value, Context context)
-			throws IOException, InterruptedException {
+	public static class TokenizerMapper
+	extends Mapper<Object, Text, Text, IntWritable>{
 
-		Text outKey = new Text(value.toString().substring(0, 1));
-		context.write(outKey, value);
+		private final static IntWritable one = new IntWritable(1);
+		private Text word = new Text();
+
+		public void map(Object key, Text value, Context context
+				) throws IOException, InterruptedException {
+			StringTokenizer itr = new StringTokenizer(value.toString());
+			while (itr.hasMoreTokens()) {
+				word.set(itr.nextToken());
+				context.write(word, one);
+			}
+		}
 	}
 
+	public static class IntSumReducer
+	extends Reducer<Text,IntWritable,Text,IntWritable> {
+		private IntWritable result = new IntWritable();
+
+		public void reduce(Text key, Iterable<IntWritable> values,
+				Context context
+				) throws IOException, InterruptedException {
+			int sum = 0;
+			for (IntWritable val : values) {
+				sum += val.get();
+			}
+			result.set(sum);
+			context.write(key, result);
+		}
+	}
+
+	public static void wcdriver(String f1, String f2, String f3, String o) throws Exception {
+		Configuration conf = new Configuration();
+		Job job = Job.getInstance(conf, "inverted index");
+		Path file1 = new Path(f1);
+		Path file2 = new Path(f2);
+		Path file3 = new Path(f3);
+		Path out = new Path(o);
+		job.setInputFormatClass(TextInputFormat.class);
+		TextInputFormat.addInputPath(job, file1);
+		TextInputFormat.addInputPath(job, file2);
+		TextInputFormat.addInputPath(job, file3);
+		job.setOutputFormatClass(TextOutputFormat.class);
+		TextOutputFormat.setOutputPath(job, out);
+		job.setJarByClass(Driver.class);
+		job.setMapperClass(WikiMapper.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(Text.class);
+		job.setReducerClass(WikiReducer.class);
+		job.waitForCompletion(true);
+	}
 }
